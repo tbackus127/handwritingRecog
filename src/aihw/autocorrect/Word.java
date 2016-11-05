15f502b2fa8dde2e9527fa76f1fa1c9b8a16b820
Eric Sakshaug <Eric.Sakshaug11@gmail.com>
2016-11-05 15:31:16 -0400

    Added levenshtein, did a bit of editing in Word, don't exactly remember what

--- src/aihw/autocorrect/Word.java
+++ src/aihw/autocorrect/Word.java
@@ -1,9 +1,34 @@
 package aihw.autocorrect;
 
+import java.util.Arrays;
+
 public class Word{
 
     String word;
 
+    //The furthese edit distance to be considered a viable replacement.
+    //5 is currently a placegholder, it should be tweaked in the future.
+    private static final int DEFAULT_THRESHOLD = 5; 
+
+    //This allows us to change the threshold for consideration
+    private int threshold;
     
+    public Word(String word){
+	threshold = DEFAULT_THRESHOLD;
+	this.word = word;
+    }
+
+    public Word(String word, int thresh){
+	threshold = thresh;
+	this.word = word;
+    }
+
+    public int getThreshold(){
+	return threshold;
+    }
+
+    public static computeDistance(Word original, String possibility){
+	
+    }
 
 }
