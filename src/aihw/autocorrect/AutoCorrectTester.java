7bccbd7951939e57c19b7be1d83f4e9abceb07c8
Eric Sakshaug <Eric.Sakshaug11@gmail.com>
2016-12-12 18:01:43 -0500

    Added AutoCorrectTester

---
+++ src/aihw/autocorrect/AutoCorrectTester.java
@@ -0,0 +1,26 @@
+package aihw.autocorrect;
+
+import aihw.autocorrect.AutoCorrect;
+
+public class AutoCorrectTester{
+
+    public static void main(String[] args){
+        if(args.length > 1){
+            String toTest = "";
+            for(int i = 0 ; i < args.length ; i++){
+                toTest += args[i];
+            }
+            System.out.println("Autocorrected " +
+                               toTest +
+                               " to " +
+                               AutoCorrect.checkString(toTest));
+        }else if(args.length == 1){
+            System.out.println("Autocorrected " +
+                               args[0] +
+                               " to " +
+                               AutoCorrect.checkWord(args[0]));
+        }else{
+            System.out.println("You must have an argument for testing!");
+        }
+    }
+}
