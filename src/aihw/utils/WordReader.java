fc640733cbf3978382bddb714f2e9872be3d69f3
Eric Sakshaug <Eric.Sakshaug11@gmail.com>
2016-12-14 01:15:59 -0500

    Added wordReader

---
+++ src/aihw/utils/WordReader.java
@@ -0,0 +1,22 @@
+package aihw.utils;
+
+import aihw.nnet;
+import java.io.File;
+
+public class WordReader{
+
+    public static void main(String[] args){
+        System.out.println("Starting to read a String from saved data");
+        if(args.length > 0){
+            //Come back to this
+        }else{
+            System.out.println(readString());
+        }
+    }
+
+    public static String readString(){
+        NNRecognizer nNet = new NNRecognizer(new HWNeuralNet(new File("res/tdata/")));
+        
+    }
+    
+}
