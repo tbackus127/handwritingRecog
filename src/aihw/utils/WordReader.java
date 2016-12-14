bd65d5923946dfed2fb8663b573870fb586ea789
unknown <Tyler_Fiacco@yahoo.com>
2016-12-14 01:27:04 -0500

    yup here you go eric

--- src/aihw/utils/WordReader.java
+++ src/aihw/utils/WordReader.java
@@ -1,22 +1,25 @@
 package aihw.utils;
 
-import aihw.nnet;
 import java.io.File;
 
-public class WordReader{
+import aihw.nnet.HWNeuralNet;
+import aihw.nnet.NNRecognizer;
 
-    public static void main(String[] args){
-        System.out.println("Starting to read a String from saved data");
-        if(args.length > 0){
-            //Come back to this
-        }else{
-            System.out.println(readString());
-        }
-    }
+public class WordReader {
 
-    public static String readString(){
-        NNRecognizer nNet = new NNRecognizer(new HWNeuralNet(new File("res/tdata/")));
-        File resFolder = new File();
+  public static void main(String[] args) {
+    System.out.println("Starting to read a String from saved data");
+    if (args.length > 0) {
+      // Come back to this
+    } else {
+      System.out.println(readString());
     }
-    
+  }
+
+  public static String readString() {
+    NNRecognizer nNet = new NNRecognizer(new HWNeuralNet(new File("res/tdata/")));
+    File resFolder = new File("res/data/word/");
+    return null;
+  }
+
 }
